12b0f2d921ca88398feb5810bcffec105971d636
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-08-04 20:52:50 -0400

    update

--- app.R
+++ app.R
@@ -3,8 +3,8 @@ library(dplyr)
 library(ggplot2)
 
 # Read data from CSV
-data <- read.csv("mm_demo.csv", stringsAsFactors = FALSE)
-data$ordine_data <- as.Date(data$ordine_data, format = "%m/%d/%Y")
+data <- read.csv("mmm_demo.csv", stringsAsFactors = FALSE)
+data$ordine_data <- as.Date(data$ordine_data, format = "%Y-%m-%d")
 
 # Calculate MMM Metrics
 data <- data %>%
@@ -14,41 +14,24 @@ data <- data %>%
 # Shiny App
 ui <- fluidPage(
   titlePanel("Media Mix Model (MMM) Analysis"),
-  sidebarLayout(
-    sidebarPanel(
-      h4("Select Date Range:"),
-      dateRangeInput("dateRange", "Choose a date range:",
-                     start = min(data$ordine_data),
-                     end = max(data$ordine_data),
-                     min = min(data$ordine_data),
-                     max = max(data$ordine_data)
-      )
-    ),
-    mainPanel(
-      plotOutput("revenuePlot"),
-      dataTableOutput("metricsTable")
-    )
+  mainPanel(
+    plotOutput("revenuePlot"),
+    dataTableOutput("metricsTable")
   )
 )
 
 server <- function(input, output) {
   
-  # Filter data based on date range
-  filtered_data <- reactive({
-    data %>%
-      filter(ordine_data >= input$dateRange[1] & ordine_data <= input$dateRange[2])
-  })
-  
   # Revenue Plot
   output$revenuePlot <- renderPlot({
-    ggplot(filtered_data(), aes(x = ordine_data, y = revenue)) +
+    ggplot(data, aes(x = ordine_data, y = revenue)) +
       geom_line() +
       labs(title = "Revenue Over Time", x = "Date", y = "Revenue")
   })
   
   # Metrics Table
   output$metricsTable <- renderDataTable({
-    filtered_data() %>%
+    data %>%
       select(ordine_data, revenue, Total_Spent, Media_Efficiency)
   })
 }
